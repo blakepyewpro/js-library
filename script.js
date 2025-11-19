effbbc3a2636f48b5b731d9efa2112906da730b2
Blake Pugh <bpugh@thumbstopper.com>
2025-11-19 16:54:18 -0500

    add remaining functionality

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const library = [];
 const displayArea = document.querySelector("main");
+const dialog = document.querySelector("dialog");
 
 const addBookButton = document.querySelector("button#create");
+addBookButton.addEventListener("click", () => dialog.showModal());
+
+const titleInput = document.querySelector("#title");
+const descInput = document.querySelector("#description");
+
+const modalConfirmBtn = document.querySelector ('button[type="submit"]');
+modalConfirmBtn.addEventListener ("click", () => {
+  if (titleInput.value.length >= 1 && descInput.value.length >= 1) {
+    addBookToLibrary(titleInput.value, descInput.value);
+    titleInput.value = "";
+    descInput.value = "";
+  } else {
+    console.log("Attempted to add book without title or description.")
+  }
+});
+
+const modalCancelBtn = document.querySelector('button[type="reset"]');
+modalCancelBtn.addEventListener("click", () => dialog.close());
 
 const deleteAllButton = document.querySelector("button#delete-all");
 deleteAllButton.addEventListener("click", () => deleteLibrary());
@@ -75,7 +94,7 @@ function Book(title, description, isExample = false) {
     selfElement.remove();
     const libIndex = library.findIndex(element => element.id === this.id)
     if (libIndex > -1) {
-      console.log("removing book id: " + library[libIndex].id);
+      console.log("removed book id: " + library[libIndex].id);
       library.splice(libIndex, 1);
     }
   }
@@ -89,7 +108,12 @@ function addBookToLibrary(title, description) {
 }
 
 function deleteLibrary() {
-  library.forEach((book) => book.deleteSelf());
+  const arrayLength = library.length;
+  for (let i = 0; i <= arrayLength; i++) {
+    library.pop();
+  }
+  console.log("Library trimmed to length " + library.length);
+  displayArea.replaceChildren();
 }
 
-addBookToLibrary("Test Add Function", "Lorem Ipsum et cetera... words go brr...")
+addBookToLibrary("Your First Book", "Lorem Ipsum et cetera...")
